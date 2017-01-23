657ecd4acbc1b6a6860b6418b5e572f46eff9b03
Oleg <M_O_E@i.ua>
2017-01-23 03:56:54 +0200

    kur

---
+++ popular.sql
@@ -0,0 +1,13 @@
+INSERT INTO Users
+VALUES('kpi.fpm.km33@gmail.com', 'rfhnjirf', 'nvbnva', 'Snvko');
+INSERT INTO Users
+VALUES('great_st@ukr.net', 'cnvbny', 'Anvb', 'Enbv');
+INSERT INTO Users
+VALUES('power@gmail.com', 'zbvnv56', 'Nbvn', 'Tnbvbn');
+
+
+INSERT INTO Foods (Food_title, Food_type, Food_datetime,Food_kind)
+VALUES('Diet', 'xudanya',  to_date('2016/12/29:5:00', 'yyyy/mm/dd:hh24:mi'), 'fsdfsd');
+INSERT INTO Foods (Food_title, Food_type, Food_datetime,Food_kind)
+VALUES('kachok7', 'nabor vesa', to_date('2017/04/3:20:00', 'yyyy/mm/dd:hh24:mi'), 'fdsfsd');
+
